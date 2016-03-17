feab7d50111b7317710acb180c88b89fb58bc217
Simon Taylor <simontaylor81@hotmail.com>
2016-03-17 22:34:08 +0100

    Toggle fullscreen on '#' key

--- remote.js
+++ remote.js
@@ -2,12 +2,18 @@
 
 (function () {
     var playPauseCode = 179;
+    var fullscreenKey = '#';    // Spare key that doesn't already do something.
     
+    // Play and Fullscreen buttons and implemented slightly differently
+    // for whatever reason.
     var playButton = document.getElementById('bottom-control-play');
+    var fullscreenButton = document.querySelector('#bottom-control-fullscreen .live-bottom-control');
     
-    document.onkeydown = onKey;
+    document.onkeydown = onKeyDown;
+    document.onkeypress = onKeyPress;
     
-    function onKey(evt) {
+    // keydown handler -- needed for non-standard keys like play/pause.
+    function onKeyDown(evt) {
         if (evt.repeat) { return; }
         
         if (evt.keyCode === playPauseCode) {
@@ -15,7 +21,21 @@
         }
     }
     
+    // keypress handler -- needed for # as my funny remote sends 3
+    // keydown events with weird codes for that, but a sensible keypress.
+    function onKeyPress(evt) {
+        if (evt.repeat) { return; }
+
+        if (event.keyCode === fullscreenKey.charCodeAt(0)) {
+            toggleFullscreen();
+        }        
+    }
+    
     function onPlayPause() {
         playButton.click();
+    }
+    
+    function toggleFullscreen() {
+        fullscreenButton.click();
     }
 })();
